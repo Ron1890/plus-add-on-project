cb2b743da95ea2d574ad7de6c4b5133f692d8a29
Ron1890 <vero.diss90@gmail.com>
2026-04-08 08:22:41 +0100

    polish js

---
+++ src/index.js
@@ -0,0 +1,42 @@
+function showSelectedCountry(event) {
+  event.preventDefault();
+
+  let selectedCountry = event.target.value;
+
+  if (selectedCountry.length > 0) {
+    alert(selectedCountry);
+  }
+
+  if (selectedCountry === "paris") {
+    let parisTime = moment()
+      .tz("Europe/Paris")
+      .format("dddd, MMMM D, YYYY H:mm:ss");
+
+    alert(
+      `Your current time zone is Europe/Paris and the current time is ${parisTime}`,
+    );
+  }
+
+  if (selectedCountry === "tokyo") {
+    let tokyoTime = moment()
+      .tz("Asia/Tokyo")
+      .format("dddd, MMMM D, YYYY H:mm:ss");
+
+    alert(
+      `Your current time zone is Asia/Tokyo and the current time is ${tokyoTime}`,
+    );
+  }
+
+  if (selectedCountry === "sydney") {
+    let sydneyTime = moment()
+      .tz("Australia/Sydney")
+      .format("dddd, MMMM D, YYYY H:mm:ss");
+
+    alert(
+      `Your current time zone is Australia/Sydney and the current time is ${sydneyTime}`,
+    );
+  }
+}
+
+let countrySelect = document.querySelector("#countries");
+countrySelect.addEventListener("change", showSelectedCountry);
