3fc9e95960cfda9dda1794b0187a7e42f8d80bf8
Ron1890 <vero.diss90@gmail.com>
2026-04-15 08:03:51 +0100

    changing solution

--- src/index.js
+++ src/index.js
@@ -1,36 +1,9 @@
 function showSelectedCity(event) {
-  event.preventDefault();
-
-  let selectedCity = event.target.value;
-
-  if (selectedCity === "paris") {
-    let parisTime = moment()
-      .tz("Europe/Paris")
-      .format("dddd, MMMM D, YYYY H:mm:ss");
-
+  if(event.target.value.length>0)
+{let currentTime= moment().tz(event.target.value).format("dddd, MMMM D, YYYY H:mm");
     alert(
-      `Your current time zone is Europe/Paris and the current time is ${parisTime}`,
-    );
-  }
-
-  if (selectedCity === "tokyo") {
-    let tokyoTime = moment()
-      .tz("Asia/Tokyo")
-      .format("dddd, MMMM D, YYYY H:mm:ss");
-
-    alert(
-      `Your current time zone is Asia/Tokyo and the current time is ${tokyoTime}`,
-    );
-  }
-
-  if (selectedCity === "sydney") {
-    let sydneyTime = moment()
-      .tz("Australia/Sydney")
-      .format("dddd, MMMM D, YYYY H:mm:ss");
-
-    alert(
-      `Your current time zone is Australia/Sydney and the current time is ${sydneyTime}`,
-    );
+      `Your current time zone is ${event.target.value} and the current time is ${currentTime}`);
+    
   }
 }
 
